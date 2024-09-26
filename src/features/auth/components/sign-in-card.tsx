c750fad3887f92960d7ea9e9b054ced297f99f7b
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-09-26 18:08:56 +0800

    test database

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -5,36 +5,41 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { SignInFlow } from "../types"
+import { useState } from "react"
 
 interface SignInCardProps {
     setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard = ({ setState } : SignInCardProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
                 <CardTitle>
-                Login to continue
+                    Login to continue
                 </CardTitle>
+                <CardDescription>
+                    Use your email or another service to continue
+                </CardDescription>
             </CardHeader>
-            <CardDescription>
-                Use your email or another service to continue
-            </CardDescription>
+            
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5 ">
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => {}}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Email"
                         type="email"
                         required
                     />
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => {}}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         placeholder="Password"
                         type="password"
                         required
@@ -67,7 +72,7 @@ export const SignInCard = ({ setState } : SignInCardProps) => {
                     </Button>
                 </div>
                 <p className="text-xs text-muted-foreground">
-                    Don&apos;t have an accont? <span onClick={() => setState("signUp")} className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
+                    Don&apos;t have an account? <span onClick={() => setState("signUp")} className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
                 </p>
             </CardContent>
         </Card>
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -5,37 +5,52 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { SignInFlow } from "../types"
+import { useState } from "react"
 
 interface SignUpCardProps {
     setState: (state: SignInFlow) => void;
 }
 
 export const SignUpCard = ({ setState } : SignUpCardProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+
     return (
         <Card className="w-full h-full p-8">
             <CardHeader className="px-0 pt-0">
                 <CardTitle>
-                Sign up to continue
+                    Sign up to continue
                 </CardTitle>
+                <CardDescription>
+                    Use your email or another service to continue
+                </CardDescription>
             </CardHeader>
-            <CardDescription>
-                Use your email or another service to continue
-            </CardDescription>
+            
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5 ">
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => {}}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         placeholder="Email"
                         type="email"
                         required
                     />
                     <Input
                         disabled={false}
-                        value=""
-                        onChange={() => {}}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         placeholder="Password"
+                        type="password"
+                        required
+                    />
+                    <Input
+                        disabled={false}
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder="Confirm password"
                         type="password"
                         required
                     />
@@ -67,7 +82,7 @@ export const SignUpCard = ({ setState } : SignUpCardProps) => {
                     </Button>
                 </div>
                 <p className="text-xs text-muted-foreground">
-                    Don&apos;t have an accont? <span onClick={() => setState("signUp")} className="text-sky-700 hover:underline cursor-pointer">Sign up</span>
+                    Already have an account? <span onClick={() => setState("signIn")} className="text-sky-700 hover:underline cursor-pointer">Sign in</span>
                 </p>
             </CardContent>
         </Card>
