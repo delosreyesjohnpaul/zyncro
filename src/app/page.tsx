714620b9ad28d7eca15c3e4809882830eb7f516b
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-09-27 12:56:25 +0800

    create workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react";
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
+  const {data, isLoading} = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+  useEffect(() => {
+    if (isLoading) return;
+    
+    if (workspaceId) {
+      console.log("Redirect to Workspace");
+    } else {
+      console.log("Open creation modal");
+    }
+  }, [workspaceId, isLoading]);
 
   return (
    <div>
-    Logged in!
     <UserButton/>
    </div>
   );
